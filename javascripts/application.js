4ad07c959e197906b6130d3cea4e417137dda918
Youssef Chaker <youssef.chaker@gmail.com>
2016-05-21 18:38:13 -0500

    Full design implementation. v1.0.0.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  $('#full-page').fullpage();
+  $('#full-page').fullpage({
+    navigation:         true,
+    navigationPosition: 'right',
+
+    onLeave: function(index, nextIndex, direction){
+      var leavingSection = $(this);
+
+      if ( index === 1 ) {
+        $('header').addClass('simple');
+      }
+    },
+
+    afterLoad: function(anchorLink, index) {
+      var loadedSection = $(this);
+
+      if ( index === 1 ) {
+        $('header').removeClass('simple');
+      }
+    },
+  });
 });
